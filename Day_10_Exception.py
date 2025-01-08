8ab74f2cbb852e30fc8d8bb7dee12edaf9096281
Subodh wadekar <subodhwadekar123@gmail.com>
2025-01-08 20:30:59 +0530

    Add logging configuration and age validation with error handling in Day_10_Exception.py

--- Day_10_Exception.py
+++ Day_10_Exception.py
@@ -39,3 +39,41 @@
 
 #Logging exception
         
+import time
+from logging import *
+
+def clearLogFiles():
+   with open(r'C:\Users\subod\Internship 2024-25\Internship_Work\MyLog.txt','w') as f:
+      f.truncate(0)
+      print("Log file is cleared")
+def configLogs():
+   with open(r'C:\Users\subod\Internship 2024-25\Internship_Work\MyLog.txt','a+') as f:
+      pass
+   basicConfig(filename=r'C:\Users\subod\Internship 2024-25\Internship_Work\MyLog.txt',level=ERROR,format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s')
+      
+def Auth_age(age):
+   if age<0:
+      raise ValueError("Value cannot be negative")
+   if age<18:
+      raise Exception("Below 18 age not allowed")
+   
+clearLogFiles()
+configLogs()
+
+
+try:
+   age=int(input("Enter your age: "))
+   Auth_age(age)
+except ValueError as Ve:
+   print(Ve)
+   error(f"{Ve} user is checking age and entered negative value",exc_info=True)
+except Exception as e:
+   error(e,exc_info=True)
+else:
+   print("Please wait for a while....\n")
+   time.sleep(5)
+   print("Age is verified Successfully\nAccess Granted")
+finally:
+   print("Code run successfully..")
+   print("Code is cleared after termination")
+
